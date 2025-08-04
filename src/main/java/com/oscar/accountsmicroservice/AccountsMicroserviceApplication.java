222519d8cca1e375daf3e7df5e95afe5e2e49121
olaguna7 <oscarlagunacaminero@gmail.com>
2025-08-04 18:03:01 +0200

    add info for openapi definition

--- src/main/java/com/oscar/accountsmicroservice/AccountsMicroserviceApplication.java
+++ src/main/java/com/oscar/accountsmicroservice/AccountsMicroserviceApplication.java
@@ -1,11 +1,27 @@
 package com.oscar.accountsmicroservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API documentation",
+                description = "Bank accounts microservice REST API documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Óscar Laguna",
+                        email = "oscarlagunacaminero@gmail.com"
+                )
+        )
+)
 public class AccountsMicroserviceApplication {
 
     public static void main(String[] args) {
